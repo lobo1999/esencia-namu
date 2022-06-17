9522a80a171fb89d2b08726ce873d7e2c96d0575
Manuel Ulate Sancho <manuel.ulate@ucrso.info>
2022-06-17 13:34:44 -0600

    Json Manifest

--- Client/src/components/destinations/Destinations.jsx
+++ Client/src/components/destinations/Destinations.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Destinations = () => {
     <div id="map">
-        <image src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHvYObiEXhY8VTbWGNGDkZahQozhsne70"></image>
+        
     </div>
 }
 
